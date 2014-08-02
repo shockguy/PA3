9a35e4292c0db849b6c492026d81e69bd62edad6
Sean Treadway <sean.k.treadway@gmail.com>
2014-08-01 20:08:56 -0400

    Struggling with best.R

---
+++ best.R
@@ -0,0 +1,6 @@
+#This is a function that returns the 
+#"Best Hospitals in a state"
+
+octype<-c("heart attack","heart failure","pneumonia")
+occol<-list(11,17,23)
+mapply(ocdict<-x,function(x) occol<-names(x))
