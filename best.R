0ec62959242935c4294eecefe0349946522bb0e1
Sean Treadway <sean.k.treadway@gmail.com>
2014-08-02 15:51:33 -0400

    Most of the mechanics are done It is not yet a function And the error messages are not coded in

--- best.R
+++ best.R
@@ -1,6 +1,24 @@
 #This is a function that returns the 
 #"Best Hospitals in a state"
 
+dir<-"./rprog-data-ProgAssignment3-data/"
+datafile<-"outcome-of-care-measures.csv"
+readin<-paste(dir,datafile,sep="")
+outcomes <- read.csv(readin,header=TRUE,colClasses="character")
+
 octype<-c("heart attack","heart failure","pneumonia")
 occol<-list(11,17,23)
-mapply(ocdict<-x,function(x) occol<-names(x))
+names(occol)<-octype
+
+head(outcomes[,unlist(occol["heart attack"])])
+
+ST<-"AL"
+
+state<-outcomes[outcomes[,7]==ST,c(2,unlist(occol["heart attack"]))]
+
+state[,2]=as.numeric(state[,2])
+
+rv<-state[c(which(state[,2]==19.6)),1]
+
+
+
